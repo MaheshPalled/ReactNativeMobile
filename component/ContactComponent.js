6febe9c2eb53b9e5bb1153d619f6de6e29bd92d3
Maheshkumar Palled <maheshmechx@gmail.com>
2020-04-25 11:43:48 -0700

    Week-1 Assignment

---
+++ component/ContactComponent.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import { View, Text, ScrollView } from 'react-native';
+import { Card } from 'react-native-elements';
+class Contact extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    static navigationOptions = {
+        title: 'Contact'
+    }
+
+    render() {
+        return (
+            <ScrollView>
+                <Card
+                    title='Contact Information'
+                    wrapperStyle={{ margin: 20 }}>
+                    <View>
+                        <Text style={{ marginBottom: 10 }}>
+                        1 Nucamp Way
+                        </Text>
+                        <Text style={{ marginBottom: 10 }}>
+                        Seattle, WA 98001
+                        </Text>
+                        <Text style={{ marginBottom: 10 }}>
+                        U.S.A.
+                        </Text>
+                        <Text style={{ marginBottom: 10 }}>
+                        Phone: 1-206-555-1234
+                        </Text>
+                        <Text style={{ marginBottom: 10 }}>
+                        Email: campsites@nucamp.co  
+                        </Text>
+                    </View>
+
+                </Card>
+
+            </ScrollView>
+        )
+    }
+}
+
+export default Contact;
